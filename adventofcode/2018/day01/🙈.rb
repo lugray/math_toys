e5c76cf2fe30e5a12a1e25dfcd8ab5937d904f4e
Lisa Ugray <lisa.ugray@shopify.com>
2019-12-02 14:35:06 -0500

    AOC moved to new repo

--- adventofcode/2018/day01/🙈.rb
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env ruby
-puts eval("0" + File.read('input').lines.map(&:chomp).join)
--- adventofcode/2019/aoc.rb
+++
@@ -1,7 +0,0 @@
-module AOC
-  class << self
-    def input
-      File.read("#{File.basename($0, '.rb')}.input")
-    end
-  end
-end
